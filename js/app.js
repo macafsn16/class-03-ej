972b470b4b8cdc9a2f860484658a5c923ec86f92
Miguel Antonio Castro Aponte <miguelcastro1978@hotmail.com>
2023-03-08 20:18:54 -0300

    modifcacion eje3

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let mydom = document;
+
+console.log(mydom);
+
+let myapp = document.getElementById("myapp")
+
+myapp.innerHTML= "Ya Casi Accedes a mi Informacion Laboral"
+
+let valor= prompt( "Cual es su nombre??");
+alert("Bienvenido(a) " + valor + " puedes acceder a mi informacion, Gracias");
